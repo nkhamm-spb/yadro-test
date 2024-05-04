8524c1b39262e43d628c53c009c9e8993608eada
Хамматов Никита Антонович <nakhammatov@edu.hse.ru>
2024-05-04 05:30:33 +0300

    Fixed

--- unit_test/tests.cpp
+++ unit_test/tests.cpp
@@ -14,21 +14,17 @@ static std::mt19937 mt(time(nullptr));
 long long SortSomeNumbers(int num, int maxSize, std::vector<int> &in, std::vector<int> &out,
                           const std::string &config = "") {
     std::fstream inputFile(inFilePath, std::ios::out | std::ios::trunc | std::ios::binary);
-
     for (int i = 0; i < num; ++i) {
         int val = static_cast<int>(mt());
         inputFile.write(reinterpret_cast<const char *>(&val), sizeof(int));
         in.push_back(val);
     }
     std::sort(in.begin(), in.end());
-
     inputFile.close();
 
     YadroTest::LineSortingEmulate LineSorter(inFilePath, outFilePath, maxSize, config);
     long long sortTime = LineSorter.MakeSort();
-
     std::fstream outputFile(outFilePath, std::ios::in | std::ios::binary);
-
     for (int i = 0, size = LineSorter.GetOutSize(); i < size; ++i) {
         int val;
         outputFile.read(reinterpret_cast<char *>(&val), sizeof(int));
@@ -40,33 +36,25 @@ long long SortSomeNumbers(int num, int maxSize, std::vector<int> &in, std::vecto
 
 TEST(YadroSortTest, EmptyFile) {
     std::vector<int> in, out;
-
     SortSomeNumbers(0, 1, in, out);
-
     ASSERT_EQ(true, out.empty());
 }
 
 TEST(YadroSortTest, Test100nums) {
     std::vector<int> in, out;
-
     SortSomeNumbers(100, 1, in, out);
-
     ASSERT_EQ(in, out);
 }
 
 TEST(YadroSortTest, Test100000nums) {
     std::vector<int> in, out;
-
     SortSomeNumbers(100000, 1, in, out);
-
     ASSERT_EQ(in, out);
 }
 
 TEST(YadroSortTest, BigMaxSize) {
     std::vector<int> in, out;
-
     SortSomeNumbers(10, 10000, in, out);
-
     ASSERT_EQ(in, out);
 }
 
